7388c3bcc6255e06be16fb11ff4e802a25774b71
Andrew S <andrews54757@gmail.com>
2017-02-21 12:18:22 -0500

    Update Linked_List.js

--- Lists/Linked_List.js
+++ Lists/Linked_List.js
@@ -1,1 +1,68 @@
+"use strict"
+/*
+No license here!
 
+*/
+
+class Root {
+ constructor() {
+   
+ }
+  forEach() {
+    
+  }
+  every() {
+   return true; 
+  }
+  delete() {
+   return false; 
+  }
+  
+}
+
+class ListNode {
+ constructor(child,node) {
+   this.CHILD = child;
+   this.NODE = node;
+ }
+ destroy() {
+  
+   this.CHILD = this.CHILD.CHILD;
+   return true;
+ }
+  forEach(call) {
+    call(this.NODE);
+    this.CHILD.forEach(call);
+    
+  }
+  delete(node) {
+    if (this.CHILD.NODE == node) return this.destroy();
+    return this.CHILD.delete(node);
+  }
+  every(call) {
+    if (!call(this.NODE)) return false;
+    return this.CHILD.forEach(call);
+  }
+  
+}
+module.exports = class LinkedList {
+ constructor() {
+ }
+  set(node) {
+    var n = new ListNode(this.CHILD,node)
+
+    this.CHILD = n;
+    return n;
+  }
+  delete(node) {
+if (this.CHILD.NODE == node) return this.destroy();
+    return this.CHILD.delete(node);
+  }
+  forEach(call) {
+    this.CHILD.forEach(call)
+  }
+  every(call) {
+    return this.CHILD.every(call)
+  }
+
+}
