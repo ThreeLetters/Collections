23d046769791f7b8231dc983bf5eb41f18662940
Andrew S <andrews54757@gmail.com>
2017-02-21 12:13:34 -0500

    Update Doubly_Linked_List.js

--- Lists/Doubly_Linked_List.js
+++ Lists/Doubly_Linked_List.js
@@ -1,1 +1,63 @@
+"use strict"
+class Root {
+ constructor() {
+   
+ }
+  delete(node) {
+   return false; 
+  }
+  forEach() {
+    
+  }
+  every() {
+   return true; 
+  }
+  
+}
 
+class ListNode {
+ constructor(child,parent,node,id) {
+   this.CHILD = child;
+   this.NODE = node;
+   this.PARENT = parent;
+ }
+ destroy() {
+  this.PARENT.CHILD = this.CHILD;
+   this.CHILD.PARENT = this.PARENT;
+   return true;
+ }
+  delete(node) {
+   if (this.NODE == node) return this.destroy();
+    return this.CHILD.delete(node);
+  }
+  forEach(call) {
+    call(this.NODE);
+    this.CHILD.forEach(call);
+    
+  }
+  every(call) {
+    if (!call(this.NODE)) return false;
+    return this.CHILD.forEach(call);
+  }
+  
+}
+module.exports = class DoublyLinkedList {
+ constructor() {
+ }
+  insert(node) {
+    var n = new ListNode(this.CHILD,this,node)
+    this.CHILD.PARENT = n;
+    this.CHILD = n;
+    return n;
+  }
+  delete(node) {
+    return this.CHILD.delete(node);
+  }
+  forEach(call) {
+    this.CHILD.forEach(call)
+  }
+  every(call) {
+    return this.CHILD.every(call)
+  }
+
+}
