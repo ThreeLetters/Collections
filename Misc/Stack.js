5c1a4f3e822bbc6548dd6752246d1bbe8531e261
Andrew S <andrews54757@gmail.com>
2017-02-21 13:04:42 -0500

    Update Stack.js

--- Misc/Stack.js
+++ Misc/Stack.js
@@ -1,1 +1,23 @@
-
+"use strict";
+module.exports = class Stack {
+  constructor() {
+   this.ARR = []; 
+  }
+  insert(node) {
+   this.ARR.push(node) 
+  }
+  remove(node) {
+   var ind = this.ARR.indexOf(node)
+   
+   if (ind == -1) return false;
+    this.ARR.splice(ind,1)
+    return true;
+  }
+  peek() {
+    return this.ARR[this.ARR.length - 1]
+  }
+  pop() {
+   this.ARR.pop() 
+  }
+    
+}
