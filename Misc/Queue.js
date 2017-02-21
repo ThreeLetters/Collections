85ab7f403f34db94e950e2b6a3d83df5487dce59
Andrew S <andrews54757@gmail.com>
2017-02-21 12:23:41 -0500

    Update Queue.js

--- Misc/Queue.js
+++ Misc/Queue.js
@@ -1,1 +1,23 @@
-
+"use strict";
+module.exports = class Queue {
+ constructor() {
+  this.ARR = []; 
+ }
+  getLen() {
+   return this.ARR.length
+  }
+ insert(node) {
+   this.ARR.push(node)
+ }
+  remove(node) {
+    if (!this.ARR.length) return false;
+    var ind = this.ARR.indexOf(node)
+    if (!ind) return false;
+    this.ARR.splice(ind,1);
+  }
+  peek() {
+    return this.ARR[0];
+  }
+  
+  
+}
